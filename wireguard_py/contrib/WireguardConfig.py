5a52317ba2a9821abb7aecd81dbdef0a260c272e
Arnav Das <arnav.das88@gmail.com>
2025-03-27 12:44:03 +0000

    commented out make_qr
    Signed-off-by: Arnav Das <arnav.das88@gmail.com>

--- wireguard_py/contrib/WireguardConfig.py
+++ wireguard_py/contrib/WireguardConfig.py
@@ -331,4 +331,4 @@ class WireguardConfig:
 
     # def to_qrcode(self) -> QRCode:
     #     config = self.to_wgconfig(wgquick_format=True)
-        return make_qr(config, mode="byte", encoding="utf-8", eci=True)
+    #     return make_qr(config, mode="byte", encoding="utf-8", eci=True)
