dfbe326d8f7ec753fa949f45dda30770f13fc026
Arnav Das <arnav.das88@gmail.com>
2025-07-23 08:43:27 +0000

    Updated Example

--- examples/wireguard_interface.py
+++ examples/wireguard_interface.py
@@ -1,37 +1,40 @@
-from wireguard_py.WireguardInterface import WireGuardInterface
+from wireguard_py.pythonic import WireGuardInterface
 from ipaddress import IPv4Address, IPv6Address, IPv4Network, IPv6Network
-
-import wireguard_py
 from wireguard_py.contrib.WireguardKey import WireguardKey
 from wireguard_py.wireguard_common import Endpoint, Peer
 
-p1key = WireguardKey("kCZHwpflKBj6tSyHkeoqlnC/31NRtgBpRUjw1u/w81Q=") # Peer Private Key
-p1 = Peer(
-    pubkey=str(p1key.public_key()), 
-    privkey=str(p1key.private_key()),
-    interface=Endpoint(ip = IPv4Address("172.10.10.2")),
-    allowed_ips=[ IPv4Network("172.10.10.0/24") ]
-)
 
-wg0 = WireGuardInterface(
-    "wg0", 
-    cidr=IPv4Network("172.10.10.0/24"),
-    keypair=WireguardKey("yO+XDIp4FP7jqjTJkYQuN6VI+r9heqoUjVSVvoGwf3w="), # Interface Private Key
-    peers={
-        "p1": p1
-    }
-)
+def main():
+    peer_key = WireguardKey("kCZHwpflKBj6tSyHkeoqlnC/31NRtgBpRUjw1u/w81Q=") # Peer Private Key
+    interface_key = WireguardKey("yO+XDIp4FP7jqjTJkYQuN6VI+r9heqoUjVSVvoGwf3w=") # Interface Private Key
 
-if wg0.status:
+    wg0 = WireGuardInterface(
+        "wg-1101", 
+        endpoint=Endpoint(ip = IPv4Address("172.10.10.1"), port=40262),
+        cidr=IPv4Network("172.10.10.0/24"),
+        keypair=interface_key,
+        peers={
+            "p1": Peer(
+                pubkey=str(peer_key.public_key()), 
+                privkey=str(peer_key.private_key()), # Not Compulsory
+                interface=Endpoint(ip = IPv4Address("172.10.10.2")),
+                allowed_ips=[ IPv4Network("172.10.10.0/24") ]
+            )
+        }
+    )
+
+    if wg0.status:
+        wg0.bring_down()
+
+    print(wg0.peers['p1'].to_config(wg0))
+
+    wg0.bring_up()
+    input("\nTunnel is Up ...")
+
+    
     wg0.bring_down()
+    input("\nTunnel is Down ...")
 
-wg0.bring_up()
-print(p1.to_config(wg0))
 
-while True:
-    e = input(">>>")
-    print(f"{e}")
-    if e == "exit\n":
-        continue
-
-wg0.bring_down()
+if __name__ == "__main__":
+    main()
